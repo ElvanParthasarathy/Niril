6c2b549b18b11b0153526e268d698f24bdd2f767
ElvanParthasarathy <jaiprakashpartha@gmail.com>
2026-06-09 13:13:29 +0530

    style: flip custom SVG sparkle icon horizontally

--- moolam/pagudhigal/CoolieBill/CoolieDashboard.tsx
+++ moolam/pagudhigal/CoolieBill/CoolieDashboard.tsx
@@ -78,8 +78,8 @@ export default function CoolieDashboard({ onViewAll, onNew, onView, onSwitchMode
     .slice(0, 6);
 
   const customSparkle = (
-    <Box component="span" sx={{ display: 'inline-flex', alignItems: 'center', ml: 0.5 }}>
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFC107" style={{ transform: 'scaleX(-1)' }}>
+    <Box component="span" sx={{ display: 'inline-flex', alignItems: 'center', ml: 0.5, mt: -0.5 }}>
+      <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#FFC107" style={{ transform: 'scaleX(-1)' }}>
         <path d="M0 0h24v24H0z" fill="none"/>
         <path d="M19 9l1.25-2.75L23 5l-2.75-1.25L19 1l-1.25 2.75L15 5l2.75 1.25L19 9zm-7.5.5L9 4 6.5 9.5 1 12l5.5 2.5L9 20l2.5-5.5L17 12l-5.5-2.5zM19 15l-1.25 2.75L15 19l2.75 1.25L19 23l1.25-2.75L23 19l-2.75-1.25L19 15z"/>
       </svg>
