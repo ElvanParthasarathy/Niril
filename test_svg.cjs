684530ccd0764d8ebde1c4896356c2e69daf8a2a
ElvanParthasarathy <jaiprakashpartha@gmail.com>
2026-06-14 22:13:40 +0530

    chore: Remove legacy dev scripts and backup files

--- test_svg.cjs
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const p = JSON.parse(fs.readFileSync('tharavu/profile.json'));
-fs.writeFileSync('test_svg.html', `<!DOCTYPE html><html><body><img src="${p.wideLogo}" style="height: 140px; max-width: 400px; object-fit: contain;" /></body></html>`);
